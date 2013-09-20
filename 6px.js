c52c14e45334b9440abc2479776f94f52f99064a
Dustin Higginbotham <dustinh052@gmail.com>
2013-09-20 11:00:02 -0600

    Added in more info to our parseInput callback (width, height, mime).  Then added in a basic dropzone convenience method

--- 6px.js
+++ 6px.js
@@ -2,6 +2,9 @@
 
 	var version = '0.0.2';
 
+	/**
+	 * Sends our data up to the 6px server
+	 */
 	var sendToServer = function(json, success, failed) {
 
 		var user = px.userData;
@@ -182,6 +185,48 @@
 
 	};
 
+	/**
+	 * Very simple dropzone creator to help with creating HTML5 file uploads.
+	 *
+	 * Allows an element on the page to be set up so you can drag a file from your computer
+	 * and have it read the file inline.
+	 */
+	px.dropZone = function(input, options) {
+
+		if (typeof input == 'string') {
+			var elm = document.querySelector(selector);
+		} else {
+			var elm = input;
+		}
+
+		var wrapCallbacks = function(e, cb) {
+			e.preventDefault();
+
+			if (cb) cb();
+
+			return false;
+		};
+ 
+	    var dragOver = function(e) {
+	        return wrapCallbacks(e, options.onDragOver);
+	    };
+	    var dragEnd = function(e) {
+	    	return wrapCallbacks(e, options.onDragOver);
+	    };
+	    var dropped = function(e) {
+	    	return wrapCallbacks(e, options.onDrop);
+	    };
+
+	    elm.ondragover = dragOver;
+	    elm.ondragend = onDragEnd;
+	    elm.ondrop = onDrop;
+	};
+
+	/**
+	 * Reads in an input of multiple types for parsing as a DataURI
+	 *
+	 * If using with dropzone, you should utilize e.dataTransfer
+	 */
 	px.parseInput = function(input, fn) {
 
 		if (typeof input == 'string') {
@@ -214,7 +259,30 @@
 
 		var dataUrlReader = new FileReader;
 		dataUrlReader.onloadend = function() {
-			fn.call(null, this.result);
+
+			var regex = /^data:.+\/(.+);base64,(.*)$/,
+				matches = this.result;
+
+			var img = new Img;
+
+			img.onload = function() {
+				/**
+				 * Arguments for your callback function (ignore the null... its for scope):
+				 * - datauri
+				 * - optional image information (width, height, mime)
+				 */
+
+				var extra = { 
+					width: img.width, 
+					height: img.height, 
+					mime: matches[1] 
+				};
+
+				fn.call(null, this.result, extra);
+			};
+
+			img.src = this.result;
+
 		};
 
 		dataUrlReader.readAsDataURL(f);
